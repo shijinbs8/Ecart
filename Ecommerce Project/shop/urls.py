6c5175059c999b82cffa971dfb3217ba8715df30
shijinbs8 <shijinbs8@gmail.com>
2022-11-25 10:25:07 +0530

    added

---
+++ Ecommerce Project/shop/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+app_name='shop'
+
+urlpatterns = [
+
+    path('',views.AllProdCat,name='AllProdCat'),
+    path('<slug:c_slug>/',views.AllProdCat,name='product_by_category'),
+    path('<slug:c_slug>/<slug:product_slug>/',views.prodetails,name='prodetails')
+
+
+]
